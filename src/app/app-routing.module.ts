6c1f0b830e024f3f898883c0f9465c0b5bfe2da6
Car18los <carlos24288077@gmail.com>
2023-06-02 22:02:34 -0500

    SUBIDA DE PROYECTO

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CategoryComponent } from './components/category/category.component';
+import { AboutComponent } from './components/about/about.component';
+const routes: Routes = [
+  {
+    path: 'home' , component: HomeComponent
+  },
+  {
+    path: 'category' , component:CategoryComponent
+  },
+  {
+    path: 'about' , component:AboutComponent
+  }
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import { CategoryComponent } from './components/category/category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BuscadorComponent,
+    AboutComponent,
+    HomeComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
