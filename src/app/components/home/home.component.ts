add8c65919a076328752d1020297944686f38a0c
CARLOS <carlos_andres1471@hotmail.com>
2023-09-18 13:23:33 -0500

    let

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,6 +2,12 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { category } from 'src/app/models/categorys.interfaz';
 import { CategoryService } from 'src/app/servicios/category.service';
+import { Series } from 'src/app/models/seriess.interfaz';
+import { SeriesService } from 'src/app/servicios/series.service';
+import { Hombres } from 'src/app/models/hombress.interfaz';
+import { HombresService } from 'src/app/servicios/hombres.service';
+import { Mujeres } from 'src/app/models/mujeress.interfaz';
+import { MujeresService } from 'src/app/servicios/mujeres.service';
 
 @Component({
   selector: 'app-home',
@@ -10,10 +16,27 @@ import { CategoryService } from 'src/app/servicios/category.service';
 })
 export class HomeComponent {
   listarCategory: category[] = [];
+  listarSeries: Series[] = [];
+  listarHombres:Hombres[] = [];
+  listarMujeres:Mujeres[] = [];
+ 
 
-  constructor(private categoryServices: CategoryService, private router:Router)
-  {
+  constructor(
+    private categoryServices: CategoryService, 
+    private router:Router, 
+    private SeriesServices: SeriesService,
+    private HombresServices:HombresService,
+    private MujeresServices:MujeresService,
+    ){
+
     this.listarCategory = this.categoryServices.getListarCategory();
+
+    this.listarSeries = this.SeriesServices.getListarSeries();
+    this.listarHombres=this.HombresServices.getListarSeries();
+    this.listarMujeres=this.MujeresServices.getListarMujeres();
+    let fondos = [this.listarSeries,  this.listarHombres,this.listarMujeres];
+    console.log(fondos)
+
   }
   
   selectedCard(id:string){
